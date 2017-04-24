7fb37470d6eff477ad0a1be4a45503e83a040e42
vishalrangras <vishalrangras@yahoo.com>
2017-04-24 18:35:34 +0530

    Added Color Threshold code

--- README.md
+++ README.md
@@ -6,7 +6,15 @@ For a lane line that is close to vertical, you can fit a line using this formula
 
 A gives you the curvature of the lane line, B gives you the heading or direction that the line is pointing, and C gives you the position of the line based on how far away it is from the very left of an image (y = 0).
 
+In the case of lane lines, we're interested only in edges of a particular orientation. So now we will explore the direction, or orientation, of the gradient.
 
+The direction of the gradient is simply the inverse tangent (arctangent) of the y gradient divided by the x gradient:
+
+arctan(sobely​ /sobel​x​​ ).
+
+Each pixel of the resulting image contains a value for the angle of the gradient away from horizontal in units of radians, covering a range of −π/2 to π/2. An orientation of 0 implies a horizontal line and orientations of +/−π/2 imply vertical lines.
+
+It's worth noting, however, that the R channel still does rather well on the white lines, perhaps even better than the S channel. As with gradients, it's worth considering how you might combine various color thresholds to make the most robust identification of the lines.
 
 # Define a function that takes an image, number of x and y points, 
 # camera matrix and distortion coefficients
@@ -44,3 +52,4 @@ def corners_unwarp(img, nx, ny, mtx, dist):
     # Return the resulting image and matrix
     return warped, M
 	
+	hls = cv2.cvtColor(im, cv2.COLOR_RGB2HLS)
---
+++ colorthreshold.py
@@ -0,0 +1,79 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+def gray(img):
+   return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def thresh(img, thresh=(0,255)):
+    binary = np.zeros_like(img)
+    binary[(img > thresh[0]) & (img <= thresh[1])] = 1
+    return binary
+
+def rgb(img):
+    #The RGB sequence is reversed as cv2.imread is used instead of mpimg.imread
+    B = img[:,:,0]
+    G = img[:,:,1]
+    R = img[:,:,2]
+    return R, G, B
+
+def hls(img):
+    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    H = hls[:,:,0]
+    L = hls[:,:,1]
+    S = hls[:,:,2]
+    return H, L, S
+
+color_threshold_plot = plt.figure(figsize=(9,18))
+
+def add_to_plot(img, label, count, color=False):
+    axis = color_threshold_plot.add_subplot(3,4,count)
+    axis.set_xlabel(label)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    if color:
+        axis.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    else:
+        axis.imshow(img, cmap="gray")
+
+path2 = "test_images/"
+file = "test6.jpg"
+
+img = cv2.imread(path2+file)
+add_to_plot(img, "Source", 1, True)
+
+gray = gray(img)
+add_to_plot(gray, "Grayscale", 2)
+
+graythresh = thresh(gray, thresh=(180,255))
+add_to_plot(graythresh, "Gray thresh", 3)
+
+R, G, B = rgb(img)
+r_thresh = thresh(R, thresh=(200,255))
+g_thresh = thresh(G, thresh=(200,255))
+b_thresh = thresh(B, thresh=(200,255))
+
+
+add_to_plot(g_thresh, "G thresh", 5)
+add_to_plot(b_thresh, "B thresh", 6)
+
+H, L, S = hls(img)
+h_thresh = thresh(H, thresh=(15,100))
+l_thresh = thresh(L, thresh=(90,255))
+s_thresh = thresh(S, thresh=(90,255))
+
+add_to_plot(l_thresh, "L thresh", 7)
+add_to_plot(h_thresh, "H thresh", 8)
+
+
+combined = np.zeros_like(h_thresh)
+combined[((r_thresh==1)&(h_thresh==1))] = 1
+
+add_to_plot(r_thresh, "R thresh", 9)
+add_to_plot(s_thresh, "S thresh", 10)
+add_to_plot(combined, "Combined R and S Thresh", 11)
+
+plt.tight_layout(h_pad=0, w_pad=0)
+plt.show()
+color_threshold_plot.savefig('output_images/color_threshold_plot.png')
